e3d77f3371e3d8ae566878979145135bcfac874a
Dick <jholarck@gmail.com>
2021-12-10 20:41:34 -0500

    update node generator

--- test/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-var assert = require('assert');
-//var loopbackSlug = require('../lib');
-describe('loopback-slug', function () {
-  it('should have unit test!', function () {
-    assert(false, 'we expected this package author to add actual unit tests.');
-  });
-});
